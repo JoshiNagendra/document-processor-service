a6bf262260b65f9cce6a1ca6f4924ae92c279b48
JoshiNagendra <joshichennamsetti999@gmail.com>
2025-06-14 23:26:50 +0530

    Initial commit for document processor service

---
+++ src/main/java/com/joshi/document_processor_service/DocumentProcessorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.joshi.document_processor_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentProcessorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentProcessorServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joshi/document_processor_service/DocumentProcessorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joshi.document_processor_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentProcessorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
